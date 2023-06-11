931d7885de0dcf354cedcf8fb745016c0068fb5a
Kingsley <divine10646@gmail.com>
2023-06-11 19:52:04 +0100

    initial layout for accounts

--- app/(site)/account/layout.tsx
+++ app/(site)/account/layout.tsx
@@ -1,18 +1,32 @@
-import { ReactNode } from "react";
+'use client'
+import { ReactNode, useState } from "react";
 
 
 
 export default function AccountsLayout({ children }: { children: ReactNode; }) {
+    const [activeTab, setActiveTab] = useState(1);
+
+    const tabs = [
+        { name: "General Settings", isActive: true },
+        { name: "My Advert", isActive: false },
+        { name: "Feedback", isActive: false },
+    ]
+
     return (
-        <section className="w-full p-5 col-center gap-5">
-            <div className="w-full shadow-lg shadow-neutral-200 center p-4">My Accounts</div>
+        <section className="w-full p-5 md:p-10 lg:p-12 col-start gap-5 md:gap-10">
+            <div className="w-full shadow shadow-neutral-200 center p-4">My Accounts</div>
             <div className="w-full text-sm">
-                <div className="w-full flex justify-start items-center gap-3">
-                    <div className="underline underline-offset-[6px] text-center">General Settings</div>
-                    <div className="underline underline-offset-[6px] text-center">My Advert</div>
-                    <div className="underline underline-offset-[6px] text-center">Feedback</div>
+                <div className="w-full lg:w-1/2 flex justify-start lg:justify-around items-center gap-3">
+                    {tabs.map((el, idx) => (
+                        activeTab === idx
+                            ? <div className={'cursor-pointer text-center pb-3 font-medium underline underline-offset-[17px] decoration-2 text-red-500'} key={idx}>{el.name}</div>
+                            : <div className={'cursor-pointer text-center pb-3 font-medium '} key={idx} onClick={() => setActiveTab(idx)}>{el.name}</div>
+                    ))}
                 </div>
                 <hr className="w-full border-b-neutral-300" />
+            </div>
+            <div className="w-full">
+                {children}
             </div>
         </section>
     )
--- app/(site)/account/settings/page.tsx
+++ app/(site)/account/settings/page.tsx
@@ -2,6 +2,15 @@
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className="w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="min-h-[400px] rounded-xl shadow"></div>
+        <div className="min-h-[400px] grid grid-flow-row gap-3">
+          <div className="min-h-[200px] shadow rounded-xl "></div>
+          <div className="min-h-[120px] shadow rounded-xl "></div>
+          <div className="min-h-[120px] shadow rounded-xl "></div>
+        </div>
+      </div>
+    </div>
   )
 }
